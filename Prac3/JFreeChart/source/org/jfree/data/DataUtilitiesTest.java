48c961e60d83fd99b1a69e143190a6165ad59f58
Adam <amcneilly08@qub.ac.uk>
2024-04-03 18:39:55 +0100

    Added Lab2 Test Suite

---
+++ Prac3/JFreeChart/source/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,179 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Values2D;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataUtilitiesTest {
+
+	private Values2D values2D;
+	private KeyedValues values;
+
+	@Before
+	public void setUp() throws Exception {
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+	    testValues.addValue(1, 0, 0);
+	    testValues.addValue(4, 1, 0);
+	    testValues.addValue(2, 0, 1);
+	    testValues.addValue(3, 1, 1);
+	    
+	    DefaultKeyedValues keyedTestValues = new DefaultKeyedValues();
+	    values = keyedTestValues;
+	    keyedTestValues.addValue("key1", 5);
+	    keyedTestValues.addValue("key2", 9);
+	    keyedTestValues.addValue("key3", 2);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		values2D = null;
+		values = null;
+
+	}
+ 
+	//----------calculateColumnTotal Tests----------
+	@Test
+	public void testCalculateColumnTotalValidData() {
+		assertEquals("CalculateColumnTotal Failed: It should be 5.0", 5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d);
+	}
+
+	@Test
+	public void testCalculateColumnTotalNull() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 0);
+			fail("No exception thrown");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test
+	public void testCalculateColumnTotalInvalidColumn() {
+		try {
+			//Checks assert output if it receives one for a failure 
+			assertEquals("CalculateColumnTotal Failed: It should return 0.0 for invalid column", 0.0, DataUtilities.calculateColumnTotal(values2D, 3), 0.0000001d);
+		}
+		catch (Exception e) {
+			//if exception is triggered this is also a fail
+			fail("Exception should not be thrown and 0.0 returned");
+		}
+	}
+	
+	//----------calulateRowTotal Tests----------
+	
+	@Test
+	public void testCalculateRowTotalValidData() {
+		assertEquals("calculateRowTotal Failed it should be 3.0", 3.0, DataUtilities.calculateRowTotal(values2D, 0),  0.0000001d);
+	}
+	
+	@Test
+	public void testCalculateRowTotalNull() {
+		try {
+			DataUtilities.calculateRowTotal(null, 0);
+			fail("No exception thrown");
+		}
+		catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
+		}
+		
+	}
+	
+	@Test
+	public void testCalculateRowTotalInvalidRow() {
+		try {
+			//Checks assert output if it receives one for a failure 
+			assertEquals("CalculateRowTotal Failed: It should return 0.0 for invalid row", 0.0, DataUtilities.calculateRowTotal(values2D, 3), 0.0000001d);
+		}
+		catch (Exception e) {
+			//if exception is triggered this is also a fail
+			fail("Exception should not be thrown and 0.0 returned");
+		}
+		
+	}
+	
+	
+	//----------createNumberArray Tests----------
+	
+	@Test
+	public void testCreateNumberArrayValidData() {
+		double[] validData = {1.0, 2.0, 3.0};
+		Number[] expected = new Number[] {1.0, 2.0, 3.0};
+		
+		assertArrayEquals("createNumberArray failed: Array does not match expected array", expected, DataUtilities.createNumberArray(validData));
+	}
+	
+	@Test
+	public void testCreateNumberArrayNull() {
+		try {
+			DataUtilities.createNumberArray(null);
+			fail("No exception thrown");
+		}
+		catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	
+	
+	//----------createNumberArray2D Tests----------
+	
+	@Test
+	public void testCreateNumberArray2DValidData() {
+		double[][] validData = {{1.0, 2.0}, {3.0, 4.0}};
+		Number[][] expected = new Number[2][2];
+		expected[0][0] = 1.0;
+		expected[0][1] = 2.0;
+		expected[1][0] = 3.0;
+		expected[1][1] = 4.0;
+		
+		assertArrayEquals("createNumberArray2D failed: Array does not match expected array", expected, DataUtilities.createNumberArray2D(validData));
+	}
+	
+	@Test
+	public void testCreateNumberArray2DNullData() {
+		try {
+			DataUtilities.createNumberArray2D(null);
+			fail("No exception thrown");
+		}
+		catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	
+	//----------getCummulativePercentages Tests----------
+	
+	@Test
+	public void testGetCummulativePercentageValidData() {
+		KeyedValues valid = DataUtilities.getCumulativePercentages(values);
+		
+		//checks all 3 parts of the arithmetic for full coverage
+		assertEquals("getCummulativePercentage Failed: Key 1 arithmetic incorrect", 0.3125, valid.getValue("key1"));
+		assertEquals("getCummulativePercentage Failed: Key 2 arithmetic incorrect", 0.875, valid.getValue("key2"));
+		assertEquals("getCummulativePercentage Failed: Key 3 arithmetic incorrect", 1.0, valid.getValue("key3"));
+	}
+	
+	@Test
+	public void testGetCummulativePercentageNullData() {
+		try {
+			DataUtilities.getCumulativePercentages(null);
+			fail("No exception thrown");
+		}
+		catch (Exception e){
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	
+	
+
+}
---
+++ Prac3/JFreeChart/source/org/jfree/data/RangeTest.java
@@ -0,0 +1,360 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest {
+
+	private Range rangeObjectUnderTest1;
+	private Range rangeObjectUnderTest2;
+	private Range rangeObjectBothNegative1;
+	private Range rangeObjectBothNegative2;
+	private Range rangeObjectBothNegativeAndEqual;
+	private Range rangeObjectBothEqual;
+
+	@Before
+	public void setUp() throws Exception {
+		rangeObjectUnderTest1 = new Range(1.0, 5.0);
+		rangeObjectUnderTest2 = new Range(6.0, 10.0);
+		rangeObjectBothNegative1 = new Range(-5.0, -1.0);
+		rangeObjectBothNegative2 = new Range(-10.0, -6.0);
+		rangeObjectBothNegativeAndEqual = new Range(-4.0, -4.0);
+		rangeObjectBothEqual = new Range(4.0, 4.0);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		rangeObjectUnderTest1 = null;
+		rangeObjectUnderTest2 = null;
+		rangeObjectBothNegative1 = null;
+		rangeObjectBothNegative2 = null;
+		rangeObjectBothNegativeAndEqual = null;
+		rangeObjectBothEqual = null;
+		
+	}
+
+	// ----------Intersects Method Tests----------
+
+	// Testing intersecting range and input values
+	@Test
+	public void testIntersectsIntersectingData() {
+		assertEquals("Intersects Method Failed: Data and Range values input intersect", true,
+				rangeObjectUnderTest1.intersects(2.0, 4.0));
+	}
+
+	// Testing not intersecting range and input values
+	@Test
+	public void testIntersectsNotIntersectingData() {
+		assertEquals("Intersects Method Failed: Data and Range values input do not intersect", false,
+				rangeObjectUnderTest1.intersects(6.0, 8.0));
+	}
+	
+	//Testing intersecting with equal range
+	@Test
+	public void testIntersectsIntersectingDataEqualRange() {
+		assertEquals("Intersecting Method Failed: Data intersects with equal Range", true, rangeObjectBothEqual.intersects(1.0, 5.0));
+	}
+	
+	//Testing not intersecting with equal range
+	@Test
+	public void testIntersectsNotIntersectingDataEqualRange() {
+		assertEquals("Intersecting Method Failed: Data doesn't intersect with equal Range", false, rangeObjectBothEqual.intersects(6.0, 8.0));
+	}
+	
+	//Testing negative range and negative intersecting values
+	@Test
+	public void testIntersectsIntersectingNegativeData() {
+		assertEquals("Intersects Method Failed: Data and Range values input intersect", true,
+				rangeObjectBothNegative1.intersects(-4.0, -1.0));
+	}
+	
+	//Testing negative range and not intersecting values
+	@Test
+	public void testIntersectsNotIntersectingNegativeData() {
+		assertEquals("Intersects Method Failed: Data and Range values input do not intersect", false,
+				rangeObjectBothNegative1.intersects(-8.0, -6.0));
+	}
+	
+	//Testing intersecting data with negative and equal range 
+	@Test
+	public void testIntersectsIntersectingDataNegativeEqualRange() {
+		assertEquals("Intersecting Method Failed: Data intersects with equal Range", true, rangeObjectBothNegativeAndEqual.intersects(-5.0, -1.0));
+	}
+	
+	//Testing not intersecting data with negative and equal range
+	@Test
+	public void testIntersectsNotIntersectingDataNegativeEqualRange() {
+		assertEquals("Intersects Method Failed: Data does not intersect with range", false, rangeObjectBothNegativeAndEqual.intersects(-10.0, -6.0));
+	}
+
+	// Testing upper and lower value equal and intersects range
+	@Test
+	public void testIntersectsEqualBoundsIntersectingRange() {
+		assertEquals("Intersects Method Failed: Equal Range does intersect with rangeObjectUnderTest1 (1.0-5.0) ", true,
+				rangeObjectUnderTest1.intersects(4.0, 4.0));
+	}
+	
+	//Testing upper and lower value equal and not intersecting 
+	@Test
+	public void testIntersectsEqualBoundsNotIntersectingRange() {
+		assertEquals("Intersects Method Failed: Equal Range does interect with rangeObjectUnderTest1 (1.0-5.0) ", false,
+				rangeObjectUnderTest1.intersects(8.0, 8.0));
+	}
+	
+	//Testing upper and lower values equal and intersecting negative
+	@Test
+	public void testIntersectsEqualBoundsIntersectingNegativeRange() {
+		assertEquals("Intersects Method Failed: Equal Range intersects with rangeObjectBothNegative1 (-5.0, -1.0) ", true,
+				rangeObjectBothNegative1.intersects(-4.0, -4.0));
+	}
+	
+	//Testing upper and lower values equal and not intersecting negative
+	@Test
+	public void testIntersectsEqualBoundsNotIntersectingNegativeRange() {
+		assertEquals("Intersects Method Failed: Equal Range intersects with rangeObjectBothNegative1 (-5.0, -1.0) ", false,
+				rangeObjectBothNegative1.intersects(-8.0, -8.0));
+	}
+	
+	
+	  
+	// ----------Testing Combine Method-----------
+
+	// Testing not null ranges
+	@Test
+	public void testCombineValidRanges() {
+		try {
+			Range combinedRange = Range.combine(rangeObjectUnderTest1, rangeObjectUnderTest2);
+
+			assertEquals("Combine method Failed: Lower Range is incorrect", 1.0, combinedRange.getLowerBound(),
+					0.000000001d);
+			assertEquals("Combine method Failed: Upper Range is incorrect", 10.0, combinedRange.getUpperBound(),
+					0.000000001d);
+		} catch (Exception e) {
+			fail("Combine method failed to create new Range");
+		}
+	}
+
+	//Testing not null ranges negative
+	@Test
+	public void testCombineValidNegativeRanges() {
+		try {
+			Range combinedRange = Range.combine(rangeObjectBothNegative1, rangeObjectBothNegative2);
+			Range expectedRange = new Range(-10.0, -1.0);
+			
+			assertEquals("Combine method failed: Ranges did not combine correctly", expectedRange, combinedRange);
+		} catch (Exception e) {
+			fail("Combine method failed to create new Range");
+		}
+	}
+	
+	
+	// Testing Range1 is null
+	@Test
+	public void testCombineRange1Null() {
+		try {
+			Range combinedRange = Range.combine(null, rangeObjectUnderTest2);
+
+			assertEquals("Combine method failed: Should return range 6.0-10.0", rangeObjectUnderTest2, combinedRange);
+		} catch (Exception e) {
+			fail("Combine method failed to create new Range");
+		}
+	}
+
+	// Testing Range2 is null
+	@Test
+	public void testCombineRange2Null() {
+		try {
+			Range combinedRange = Range.combine(rangeObjectUnderTest1, null);
+
+			assertEquals("Combine method failed: Should return range 1.0-5.0", rangeObjectUnderTest1, combinedRange);
+		} catch (Exception e) {
+			fail("Combine method failed to create new Range");
+		}
+	}
+
+	// Testing Both Null
+	@Test
+	public void testCombineBothRangesNull() {
+		try {
+			Range combinedRange = Range.combine(null, null);
+
+			assertEquals("Combine method failed: Should return null", null, combinedRange);
+		} catch (Exception e) {
+			fail("Combine method failed to create new Range");
+		}
+	}
+	
+
+	// ----------Testing Constrain Method----------
+
+	// Testing Constrain with values inside rangeObjectUnderTest1
+	@Test
+	public void testConstrainInsideRange() {
+		assertEquals("Constrain Method Failed: Should return 4.0", 4.0, rangeObjectUnderTest1.constrain(4.0),
+				0.000000001d);
+	}
+
+	// Testing Constrain with values below rangeObjectUnderTest1
+	@Test
+	public void testConstrainOutsideRangeBelow() {
+		assertEquals("Constrain Method Failed: Should return 1.0", 1.0, rangeObjectUnderTest1.constrain(0.0),
+				0.000000001d);
+	}
+
+	// Testing Constrain with values above rangeObjectUnderTest1
+	@Test
+	public void testConstrainOutsideRangeAbove() {
+		assertEquals("Constrain Method Failed: Should return 5.0", 5.0, rangeObjectUnderTest1.constrain(6.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values inside rangeObjectBothNegative1
+	@Test
+	public void testConstrainInsideNegativeRange() {
+		assertEquals("Constrain Method Failed: Should return -4.0", -4.0, rangeObjectBothNegative1.constrain(-4.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values below rangeObjectBothNegative1
+	public void testConstrainBelowNegativeRange() {
+		assertEquals("Constrain Method Failed: Should return -1.0", -1.0, rangeObjectBothNegative1.constrain(0.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values above rangeObjectBothNegative1
+	public void testConstrainAboveNegativeRange() {
+		assertEquals("Constrain Method Failed: Should return -5.0", -5.0, rangeObjectBothNegative1.constrain(-6.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values inside Equal range
+	public void testConstrainInsideEqualRange() {
+		assertEquals("Constrain Method Failed: Should return 4.0", 4.0, rangeObjectBothEqual.constrain(4.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values above equal range
+	public void testConstrainAboveEqualRange() {
+		assertEquals("Constrain Method Failed: Should return 4.0", 4.0, rangeObjectBothEqual.constrain(6.0),
+				0.000000001d);
+	}
+	
+	//Testing Constrain with values below equal range
+	public void testConstrainBelowEqualRange() {
+		assertEquals("Constrain Method Failed: Should return 4.0", 4.0, rangeObjectBothEqual.constrain(3.0),
+				0.000000001d);
+	}
+	
+
+	
+	// ----------Testing Expand To Include Method----------
+
+	// Testing a valid range and including value above upper bound
+	@Test
+	public void testExpandToIncludeAboveRangeValue() {
+		Range expanded = new Range(6.0, 12.0);
+
+		assertEquals("ExpandToInclude Method Failed: Should return the expanded range 6.0-12.0", expanded,
+				Range.expandToInclude(rangeObjectUnderTest2, 12.0));
+	}
+
+	// Testing a valid range and including a value below lower bound
+	@Test
+	public void testExpandToIncludeBelowRangeValue() {
+		Range expanded = new Range(4.0, 10.0);
+
+		assertEquals("ExpandToInclude Method Failed: Should return the expanded range 4.0-10.0", expanded,
+				Range.expandToInclude(rangeObjectUnderTest2, 4.0));
+	}
+
+	// Testing a valid range and including a value already within the range
+	@Test
+	public void testExpandToIncludeInsideRangeValue() {
+		assertEquals("ExpandToInclude Method Failed: Should return rangeObjectUnderTest2 range (6.0 - 10.0)",
+				rangeObjectUnderTest2, Range.expandToInclude(rangeObjectUnderTest2, 8.0));
+	}
+
+	// Testing a null range and including a value
+	@Test
+	public void testExpandToIncludeNullRange() {
+		Range expectedRange = new Range(5.0, 5.0);
+		
+		assertEquals("ExpandToInclude Method Failed: Should return rangeObjectUnderTest1 range (1.0 - 5.0)",
+				expectedRange, Range.expandToInclude(null, 5.0));
+	}
+	
+	//Testing a negative range and including a value below that range
+	@Test
+	public void testExpandToIncludeBelowNegativeRange() {
+		Range expanded = new Range(-12.0, -6.0);
+
+		assertEquals("ExpandToInclude Method Failed: Should return the expanded range -12.0, -6.0", expanded,
+				Range.expandToInclude(rangeObjectBothNegative2, -12.0));
+	}
+	
+	//Testing a negative range and including a value above that range
+		@Test
+		public void testExpandToIncludeAboveNegativeRange() {
+			Range expanded = new Range(-10.0, -4.0);
+
+			assertEquals("ExpandToInclude Method Failed: Should return the expanded range -10.0, -4.0", expanded,
+					Range.expandToInclude(rangeObjectBothNegative2, -4.0));
+		}
+	
+	
+	//Testing a negative range and including a value inside that range
+	@Test
+	public void testExpandToInlcudeInsideNegativeRange() {
+		assertEquals("ExpandToInclude Method Failed: Should return rangeObjectBothNegative2 range (-10.0, -6.0)",
+				rangeObjectBothNegative2, Range.expandToInclude(rangeObjectBothNegative2, -8.0));
+	}
+	
+	
+	
+	
+	//----------Testing Contains Method----------
+	
+	//Testing Valid range and value inside range
+	@Test
+	public void testContainsValueInsideRange() {
+		assertEquals("Contains Method Failed: Value within range should return true", true, rangeObjectUnderTest1.contains(3.0));
+	}
+	
+	//Testing Valid range and value outside range
+	@Test
+	public void testContainsValueOutsideRange() {
+		assertEquals("Contains Method Failed: Value outside range should return false", false, rangeObjectUnderTest1.contains(7.0));
+	}
+	
+	//Testing equal range and inside range
+	@Test 
+	public void testContainsValueWithinEqualRange() {
+		assertEquals("Contains Method Failed: Value within Equal range", true, rangeObjectBothEqual.contains(4.0));
+	}
+	
+	//Testing equal range and outside range 
+	@Test
+	public void testContainsValueOutsideEqualRange() {
+		assertEquals("Contains Method Failed: Value within range should return true", true, rangeObjectBothEqual.contains(8.0));
+	}
+	
+	//Testing Valid negative range and value inside range
+	@Test
+	public void testContainsValueWithinNegativeRange() {
+		assertEquals("Contains Method Failed: Value within range should return true", true, rangeObjectBothNegative1.contains(-3.0));
+	}
+	
+	//Testing valid negative range and value outside range
+	@Test
+	public void testContainsValueOutsideNegativeRange() {
+		assertEquals("Contains Method Failed: Value outside range should return false", false, rangeObjectBothNegative1.contains(-7.0));
+	}
+
+	
+	
+
+}
